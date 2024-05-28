c1dfad13d27d6bbc13af52cd3989e8abacce7483
dannhanong <thannong0512@gmail.com>
2024-05-28 14:51:57 +0700

    first

---
+++ src/main/java/com/cntt/rentalmanagement/controller/FollowController.java
@@ -0,0 +1,27 @@
+package com.cntt.rentalmanagement.controller;
+
+import com.cntt.rentalmanagement.domain.payload.request.FollowRequest;
+import com.cntt.rentalmanagement.services.FollowService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/follow")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class FollowController {
+
+    private final FollowService followService;
+
+    @PostMapping
+    public ResponseEntity<?> followAgents(@RequestBody FollowRequest followRequest){
+        return ResponseEntity.ok(followService.addFollow(followRequest));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllAgents(@RequestParam Integer pageNo,
+                                          @RequestParam Integer pageSize) {
+        return ResponseEntity.ok(followService.getAllFollowOfCustomer(pageNo, pageSize));
+    }
+}
